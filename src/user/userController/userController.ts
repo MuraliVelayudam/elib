63c4f082e13c41bdd1d8ae2501e3a661b9c16922
MuraliVelayudam <murali.velayudam@gmail.com>
2024-05-09 23:59:29 +0530

    Error Handling Added

--- src/user/userController/userController.ts
+++ src/user/userController/userController.ts
@@ -4,6 +4,7 @@ import createHttpError from 'http-errors'
 import bcrypt from 'bcrypt'
 import { config } from '../../config/config'
 import { sign } from 'jsonwebtoken'
+import { UserInterface } from '../userType'
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const { name, email, password } = req.body
@@ -13,22 +14,32 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
         next(error)
     }
 
-    const user = await User.findOne({ email })
+    try {
+        const user = await User.findOne({ email })
 
-    if (user) {
-        const error = createHttpError(400, 'User Already Exists')
-        next(error)
+        if (user) {
+            const error = createHttpError(400, 'User Already Exists')
+            next(error)
+        }
+    } catch (err) {
+        return next(createHttpError(500, 'Error in getting User'))
     }
 
     const hashedPassword = await bcrypt.hash(password, 10)
 
-    const newUser = await User.create({
-        name,
-        email,
-        password: hashedPassword,
-    })
+    let newUser: UserInterface
 
-    const token = sign({ sub: newUser._id }, config.secretKey as string, { expiresIn: '1d' })
+    try {
+        newUser = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+        })
+    } catch (err) {
+        return next(createHttpError(500, 'Error Occurred While Create a New User'))
+    }
+
+    const token = sign({ sub: newUser._id }, config.secretKey as string, { expiresIn: '1d', algorithm: 'HS256' })
 
     res.status(201).json({ accessToken: token })
 }
