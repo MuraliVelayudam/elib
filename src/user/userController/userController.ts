839a15eef29c799e705798d62648d41194fff8b0
MuraliVelayudam <murali.velayudam@gmail.com>
2024-05-10 15:41:24 +0530

    Add User Login

--- src/user/userController/userController.ts
+++ src/user/userController/userController.ts
@@ -1,19 +1,23 @@
 import { Request, Response, NextFunction } from 'express'
-import User from '../userModel/userModel'
-import createHttpError from 'http-errors'
-import bcrypt from 'bcrypt'
 import { config } from '../../config/config'
 import { sign } from 'jsonwebtoken'
 import { UserInterface } from '../userType'
+import User from '../userModel/userModel'
+import createHttpError from 'http-errors'
+import bcrypt from 'bcrypt'
+
+// User Register
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const { name, email, password } = req.body
 
+    // Checking All Required Fields
     if (!name || !email || !password) {
         const error = createHttpError(400, 'Required All Field')
         next(error)
     }
 
+    // Checking User Exists or Not
     try {
         const user = await User.findOne({ email })
 
@@ -25,8 +29,10 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
         return next(createHttpError(500, 'Error in getting User'))
     }
 
+    // Hashed Password
     const hashedPassword = await bcrypt.hash(password, 10)
 
+    // Creating New User
     let newUser: UserInterface
 
     try {
@@ -39,9 +45,54 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
         return next(createHttpError(500, 'Error Occurred While Create a New User'))
     }
 
-    const token = sign({ sub: newUser._id }, config.secretKey as string, { expiresIn: '1d', algorithm: 'HS256' })
+    // Creating Token
 
-    res.status(201).json({ accessToken: token })
+    try {
+        const token = sign({ sub: newUser._id }, config.secretKey as string, {
+            expiresIn: '1d',
+            algorithm: 'HS256',
+        })
+        res.status(201).json({ accessToken: token })
+    } catch (err) {
+        return next(createHttpError(500, 'Error occurred Sign in JWT Token'))
+    }
 }
 
-export { createUser }
+// User Login
+
+const userLogin = async (req: Request, res: Response, next: NextFunction) => {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        const errors = createHttpError(400, 'Required All Fields')
+        return next(errors)
+    }
+
+    const user = await User.findOne({ email })
+
+    try {
+        if (!user) {
+            const errors = createHttpError(404, 'User Not Exists')
+            next(errors)
+        }
+    } catch (err) {
+        return next(createHttpError(400, 'Error Occurred While Getting User'))
+    }
+
+    const isMatched = user ? await bcrypt.compare(password, user.password) : null
+
+    if (!isMatched) {
+        const errors = createHttpError(400, 'Email or Password Wrong')
+        return next(errors)
+    }
+
+    try {
+        const token = sign({ sub: user?._id }, config.secretKey as string, { expiresIn: '1d', algorithm: 'HS256' })
+
+        res.status(200).json({ accessToken: token })
+    } catch (err) {
+        return next(createHttpError(400, 'Error Occurred While Sign in  Token'))
+    }
+}
+
+export { createUser, userLogin }
--- src/user/userRouter/userRouter.ts
+++ src/user/userRouter/userRouter.ts
@@ -1,8 +1,9 @@
 import express from 'express'
-import { createUser } from './../userController/userController'
+import { createUser, userLogin } from './../userController/userController'
 
 const userRouter = express.Router()
 
 userRouter.post('/register', createUser)
+userRouter.post('/login', userLogin)
 
 export default userRouter
